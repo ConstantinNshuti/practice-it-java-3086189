ab252d0a4df6d98da1924d0f0782d4a41cd03d50
Constantin <92950536+ConstantinNshuti@users.noreply.github.com>
2024-06-21 13:28:23 +0000

    Final

--- _03_03e/Main.java
+++ _03_03e/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     GradingSystem gradingSystem = new GradingSystem();
     int percentage = 85;
+    
     System.out.println("Percentage: " + percentage);
     System.out.println("Pass: " + gradingSystem.isAPass(percentage));
     System.out.println("Grade: " + gradingSystem.getGrade(percentage));
